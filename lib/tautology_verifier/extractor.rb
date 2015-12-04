00d3d94c71470f4dbcea48109efeb1136eb6f0d2
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2015-12-04 12:26:54 +0530

    Predicates extraction through extractor class, passing test case

--- lib/tautology_verifier/extractor.rb
+++ lib/tautology_verifier/extractor.rb
@@ -1,7 +1,8 @@
 class Extractor
 
     def self.extract_predicates(statement)
-
+      predicate_array = statement.gsub(/[()!&|]/, '').gsub(/\s+/, "").split('').compact.uniq
+      predicate_array
     end
 
 end
--- spec/tautology_verifier/extractor_spec.rb
+++ spec/tautology_verifier/extractor_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe Extractor, "#extract_predicates" do
   context "when a valid propositional statement is given" do
 
     it "returns an array of predicates in statement" do
-      predicate_array = Extractor.extract_predicates('(a | b)')
-      expect(predicate_array).to eq "['a', 'b']"
+      predicate_array = Extractor.extract_predicates('((a | b) & c)')
+      expect(predicate_array).to eq ["a", "b", "c"]
     end
 
   end
