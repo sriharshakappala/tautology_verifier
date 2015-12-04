39f16ce5b633185a005147ba0a7f10cbe4550738
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2015-12-04 13:04:04 +0530

    Assumer which assigns random truth values to predicates

--- spec/tautology_verifier/assumer_spec.rb
+++ spec/tautology_verifier/assumer_spec.rb
@@ -5,8 +5,10 @@ RSpec.describe Assumer, "#assign_random_truth_values_to_predicates" do
   context "when a valid predicate array is given" do
 
     it "returns a hash with each predicate assigned to a random truth value" do
-      predicate_array = Assumer.assign_random_truth_values_to_predicates(['a', 'b'])
-      expect(predicate_array).to eq "{ 'a' => true, 'b' => false }"
+      random_truth_table = Assumer.assign_random_truth_table(['a', 'b'])
+      random_truth_table.each do |predicate_hash|
+        expect(random_truth_table[predicate_hash[0]]).not_to eq nil
+      end
     end
 
   end
