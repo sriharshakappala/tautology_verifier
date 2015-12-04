134cd9a47ccbd74677b6fdf2132205066f0103ba
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2015-12-04 15:08:03 +0530

    Formatting

--- lib/tautology_verifier/assumer.rb
+++ lib/tautology_verifier/assumer.rb
@@ -1,12 +1,12 @@
 class Assumer
 
-    def self.assign_random_truth_table(predicate_array)
-      random_truth_values = {}
-      predicate_array.each do |predicate|
-        random_truth_value = [true, false].sample
-        random_truth_values[predicate] = random_truth_value
-      end
-      random_truth_values
+  def self.assign_random_truth_table(predicate_array)
+    random_truth_values = {}
+    predicate_array.each do |predicate|
+      random_truth_value = [true, false].sample
+      random_truth_values[predicate] = random_truth_value
     end
+    random_truth_values
+  end
 
 end
