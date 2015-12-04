b8149f53e6db3078a17d573c1618ffc9226269f4
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2015-12-04 14:56:53 +0530

    Simulator passing test cases

--- lib/tautology_verifier/simulator.rb
+++ lib/tautology_verifier/simulator.rb
@@ -1,7 +1,11 @@
 class Simulator
 
   def self.substitute_truth_values_into_statement(propositional_statement, random_truth_values)
-    
+    simulated_statement = ""
+    random_truth_values.each do |truth_value|
+      simulated_statement = propositional_statement.gsub!(truth_value[0], truth_value[1].to_s)
+    end
+    simulated_statement
   end
 
 end
