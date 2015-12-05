869a44e9994ce234097ceaf9f44ae9a7115a0ecb
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2015-12-05 17:14:00 +0530

    New implementation of Simulator

--- lib/tautology_verifier/parser.rb
+++ lib/tautology_verifier/parser.rb
@@ -8,7 +8,9 @@ class Parser
   def self.parse_statement(propositional_statement)
     predicate_array = Extractor.extract_predicates(propositional_statement)
     random_truth_values = Assumer.assign_random_truth_table(predicate_array)
-    simulated_statement = Simulator.substitute_truth_values_into_statement(propositional_statement, random_truth_values)
+    possible_truth_tables = Assumer.generate_possible_truth_tables(predicate_array)
+    simulator = Simulator.new(propositional_statement, predicate_array, random_truth_values, possible_truth_tables).check_for_tautology
+    # simulated_statement = Simulator.substitute_truth_values_into_statement(propositional_statement, random_truth_values)
     decision = Decider.decide_whether_tautology(simulated_statement)
     decision
   end
--- lib/tautology_verifier/simulator.rb
+++ lib/tautology_verifier/simulator.rb
@@ -1,4 +1,35 @@
+require 'pry'
+require_relative 'decider'
+
 class Simulator
+
+  def initialize(propositional_statement, predicate_array, random_truth_values, possible_truth_tables)
+    @propositional_statement = propositional_statement
+    @predicate_array = predicate_array
+    @random_truth_values = random_truth_values
+    @possible_truth_tables = possible_truth_tables
+  end
+
+  def check_for_tautology
+    random_simultated_statment = ""
+    @random_truth_values.each do |random_truth_value|
+      random_simultated_statment = @propositional_statement.gsub!(random_truth_value[0], random_truth_value[1].to_s)
+    end
+    random_decision = Decider.decide_whether_tautology(random_simultated_statment)
+    binding.pry
+    if random_decision == false
+      "False"
+    elsif random_decision == true
+      binding.pry
+      @possible_truth_tables.each do |possible_truth_table|
+        simulated_statement = ""
+        possible_truth_table.each do |possible_truth_value|
+          simulated_statement = @propositional_statement.gsub!(random_truth_value[0], random_truth_value[1].to_s)
+        end
+        decision = Decider.decide_whether_tautology(simultated_statment)
+      end
+    end
+  end
 
   def self.substitute_truth_values_into_statement(propositional_statement, random_truth_values)
     simulated_statement = ""
