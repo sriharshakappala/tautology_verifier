c0a5c0c48b7bd5d7310b9bc6c9b0bef8ad78a90c
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2015-12-05 23:40:57 +0530

    Exiting the loop if a single combination results in false

--- lib/tautology_verifier/parser.rb
+++ lib/tautology_verifier/parser.rb
@@ -1,5 +1,4 @@
 require_relative 'assumer'
-require_relative 'decider'
 require_relative 'extractor'
 require_relative 'simulator'
 
@@ -9,9 +8,7 @@ class Parser
     predicate_array = Extractor.extract_predicates(propositional_statement)
     random_truth_values = Assumer.assign_random_truth_table(predicate_array)
     possible_truth_tables = Assumer.generate_possible_truth_tables(predicate_array)
-    simulator = Simulator.new(propositional_statement, predicate_array, random_truth_values, possible_truth_tables).check_for_tautology
-    # simulated_statement = Simulator.substitute_truth_values_into_statement(propositional_statement, random_truth_values)
-    decision = Decider.decide_whether_tautology(simulated_statement)
+    decision = Simulator.new(propositional_statement, predicate_array, random_truth_values, possible_truth_tables).check_for_tautology
     decision
   end
 
--- lib/tautology_verifier/simulator.rb
+++ lib/tautology_verifier/simulator.rb
@@ -1,4 +1,3 @@
-require 'pry'
 require_relative 'decider'
 
 class Simulator
@@ -8,27 +7,26 @@ class Simulator
     @predicate_array = predicate_array
     @random_truth_values = random_truth_values
     @possible_truth_tables = possible_truth_tables
+    @max_truth_tables = possible_truth_tables.count
   end
 
   def check_for_tautology
-    random_simultated_statment = ""
-    @random_truth_values.each do |random_truth_value|
-      random_simultated_statment = @propositional_statement.gsub!(random_truth_value[0], random_truth_value[1].to_s)
-    end
-    random_decision = Decider.decide_whether_tautology(random_simultated_statment)
-    binding.pry
+    counter = 0
+    random_decision = Decider.new(@propositional_statement, @random_truth_values).evaluate_expression
     if random_decision == false
-      "False"
+      what_simulator_tells = "False"
     elsif random_decision == true
-      binding.pry
       @possible_truth_tables.each do |possible_truth_table|
-        simulated_statement = ""
-        possible_truth_table.each do |possible_truth_value|
-          simulated_statement = @propositional_statement.gsub!(random_truth_value[0], random_truth_value[1].to_s)
+        counter += 1
+        decision = Decider.new(@propositional_statement, possible_truth_table).evaluate_expression
+        if decision == false
+          what_simulator_tells = "False"
+        elsif decision == true && counter == @max_truth_tables
+          what_simulator_tells = "True"
         end
-        decision = Decider.decide_whether_tautology(simultated_statment)
       end
     end
+    what_simulator_tells
   end
 
   def self.substitute_truth_values_into_statement(propositional_statement, random_truth_values)
