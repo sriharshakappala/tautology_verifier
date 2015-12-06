099fe0a799b164a4aa749512a416f2cc54ace861
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2015-12-06 16:54:09 +0530

    Test case for checking the possible combinations count from Assumer

--- spec/tautology_verifier/assumer_spec.rb
+++ spec/tautology_verifier/assumer_spec.rb
@@ -14,3 +14,16 @@ RSpec.describe Assumer, "#assign_random_truth_values_to_predicates" do
   end
 
 end
+
+RSpec.describe Assumer, "#generate_possible_truth_tables" do
+
+  context "when a valid predicate array is given" do
+
+    it "returns all the possible truth tables that can be formed via those predicates" do
+      possible_truth_tables = Assumer.generate_possible_truth_tables(['a', 'b'])
+      expect(possible_truth_tables.count).to eq 4
+    end
+
+  end
+
+end
