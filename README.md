8a3a36cc9f5b9764d91d14ad95adcc8699b49eae
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2015-12-06 16:33:35 +0530

    README and test case for new simulator method

---
+++ README.md
@@ -0,0 +1,21 @@
+# Tautology Verifier
+
+## How to run?
+
+    $ git clone https://github.com/sriharshakappala/tautology_verifier.git
+
+    $ cd tautology_verifier/lib
+
+    $ ruby tautology_verifier.rb
+
+## Sample Interaction
+
+    How many propositional statements do you want to check?
+    3
+    Type the propositional statement. Once you type a statement press `Enter`
+    a|b
+    a&b
+    a|!a
+    False
+    False
+    True
--- spec/tautology_verifier/simulator_spec.rb
+++ spec/tautology_verifier/simulator_spec.rb
@@ -1,12 +1,30 @@
 require_relative '../../lib/tautology_verifier/simulator'
 
-RSpec.describe Assumer, "#substitute_truth_values_into_statement" do
+RSpec.describe Simulator, "#substitute_truth_values_into_statement" do
 
   context "when predicate array and random truth table values are given" do
 
-    it "returns a simulated version of propositional statment with truth values" do
+    it "returns a simulated version of propositional statement with truth values" do
       simulated_statement = Simulator.substitute_truth_values_into_statement("((a | b) & c)", {"a"=>true, "b"=>true, "c"=>false})
       expect(simulated_statement).to eq "((true | true) & false)"
+    end
+
+  end
+
+end
+
+RSpec.describe Simulator, "#check_for_tautology" do
+
+  context "when a valid propositional statement, predicate array, random truth values and all possible truth values are given" do
+
+    it "tells whether the propositional statement is a tautology or not" do
+      decision = Simulator.new(
+        "a&b",
+        ["a", "b"],
+        {"a"=>false, "b"=>true},
+        [{"a"=>true, "b"=>true}, {"a"=>true, "b"=>false}, {"a"=>false, "b"=>true}, {"a"=>false, "b"=>false}]
+      ).check_for_tautology
+      expect(decision).to eq "False"
     end
 
   end
